92332f1cacbae43b85e085e096d2352bbf146be1
Karine Yasmin <karine.yasmin@outlook.com>
2024-09-17 19:52:29 -0300

    add: secret key

--- mercado/__init__.py
+++ mercado/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from mercado import routes
 
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
+app.config["SECRET_KEY"] = "89a6edc0e506748ec7aa9863"
 db.init_app(app)
-
-from mercado import routes
