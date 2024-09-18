8f3747e2585e2f5d994487a253eec177be268da5
Karine Yasmin <karine.yasmin@outlook.com>
2024-09-18 15:19:13 -0300

    updated

--- mercado/forms.py
+++ mercado/forms.py
@@ -4,7 +4,7 @@ from wtforms import StringField, PasswordField, SubmitField
 
 class CadastroForm(FlaskForm):
     usuario = StringField(label="Username:")
-    email = StringField(label="E-mail")
-    senha1 = PasswordField("Senha:")
-    senha = PasswordField(label="Confirmação de Senha")
+    email = StringField(label="E-mail:")
+    senha1 = PasswordField(label="Senha:")
+    senha2 = PasswordField(label="Confirmação de Senha:")
     submit = SubmitField(label="Cadastrar")
