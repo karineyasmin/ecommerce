84172a6e3905a3c3fa33bc30fcdbeb15bdd06d1f
Karine Yasmin <karine.yasmin@outlook.com>
2024-09-18 15:36:37 -0300

    updated

--- mercado/routes.py
+++ mercado/routes.py
@@ -1,7 +1,8 @@
 from mercado import app
 from flask import render_template
-from mercado.models import Item
+from mercado.models import Item, User
 from mercado.forms import CadastroForm
+from mercado import db
 
 
 @app.route("/")
@@ -15,7 +16,18 @@ def page_produto():
     return render_template("produtos.html", itens=itens)
 
 
-@app.route("/cadastro")
+@app.route("/cadastro", methods=["GET", "POST"])
 def page_cadastro():
-    form = CadastroForm
+    form = CadastroForm()
+    if form.validate_on_submit():
+        usuario = User(
+            usuario=form.usuario.data,
+            email=form.email.data,
+            senha=form.senha1.data,
+        )
+
+        db.session.add(usuario)
+        db.session.commit()
+
+        return redirect(url_for("page_produto"))
     return render_template("cadastro.html", form=form)
