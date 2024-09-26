cfb6023b8155fae5f3c8ab4d782c3158488c390e
Karine Yasmin <karine.yasmin@outlook.com>
2024-09-25 21:19:07 -0300

    updated

--- mercado/__init__.py
+++ mercado/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
 app.config["SECRET_KEY"] = "89a6edc0e506748ec7aa9863"
 db.init_app(app)
+bcrypt = Bcrypt(app)
 
 from mercado import routes
